7a2fbcba1b97ccdaa4e20ff0e250d9d6ef844d1b
solange11131719 <167382822+solange11131719@users.noreply.github.com>
2024-08-27 23:08:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const nomeCompleto = "solange generoso";
+const anoNascimento = 1976;
+let anoAtual = 2024;
+let idade = 48;
+
+
+console.log("Olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " e sou nascido no ano " + anoNascimento);
+
+anoAtual = anoAtual + 1;
+
+console.log("Olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " cursar direito ");
+
+
+
+const listaDeViagens = new Array(
+  `Rio de janeiro`,
+  '' Veneza,''
+  "Ṕaris, "
+ "" Ńova york"
+)
+
+con
+
+console.LOG(listaDeviagens[4])
+listaDeViagens.push("Coronel Vivida';  C
+console.log(listaDeviagens);
+                    
+listaDeViagens.spllcel(1,1);
+console.log(listaDeviagens);
+
+const idadeComprador =24;
+
+
+if(idadeCompradpr < 18){}
+console.log("infelismente voçe é menor de idade não podemos vender passagens")
+  if(idadeComprador >= 18){}
+console.log("Parabẽns voçẽ adiquiriu sua passagem com sussesso");
