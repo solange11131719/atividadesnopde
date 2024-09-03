f4a49017005eb519c5a7477bffa9eff72a69f398
solange11131719 <167382822+solange11131719@users.noreply.github.com>
2024-09-03 23:05:53 +0000

    03/09/2024

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-const nomeCompleto = "solange generoso";
+const nomeCompleto = "Solange Sales Generoso Dos Santos";
 const anoNascimento = 1976;
 let anoAtual = 2024;
 let idade = 48;
 
-
-console.log("Olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " e sou nascido no ano " + anoNascimento);
+console.log("Olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " e sou nascida no ano de " + anoNascimento + ".");
 
 anoAtual = anoAtual + 1;
+idade = idade + 1;
+console.log("Olá meu nome ainda é " + nomeCompleto + "," + " estamos no ano de " + anoAtual + " e finalmente fiz " + idade + " anos, " + " como eu sou nascida em 1976 ano que vem faço 48 anos e as minhas responsabilidades aumentam.");
 
-console.log("Olá eu sou " + nomeCompleto + " estamos no ano " + anoAtual + " minha idade é " + idade + " cursar direito ");
-
-
+console.log(`oi eu sou ${nomeCompleto} meu ano de nascimento é ${anoNascimento} meu ano atual é ${anoAtual} e minha idade é ${idade}`);
 
 const listaDeViagens = new Array(
-  `Rio de janeiro`,
-  '' Veneza,''
-  "Ṕaris, "
- "" Ńova york"
-)
+`Mexico` ,
+`Canada`,
+`Estados Unidos`,
+`Alemanha`,
+`Italia`,
+);
 
-con
+console.log(listaDeViagens);
 
-console.LOG(listaDeviagens[4])
-listaDeViagens.push("Coronel Vivida';  C
-console.log(listaDeviagens);
-                    
-listaDeViagens.spllcel(1,1);
-console.log(listaDeviagens);
+console.log(listaDeViagens[4]);
 
-const idadeComprador =24;
+listaDeViagens.push("Coronel Vivida");
+console.log(listaDeViagens);
 
+listaDeViagens.splice(1,1);
+console.log(listaDeViagens);
 
-if(idadeCompradpr < 18){}
-console.log("infelismente voçe é menor de idade não podemos vender passagens")
-  if(idadeComprador >= 18){}
-console.log("Parabẽns voçẽ adiquiriu sua passagem com sussesso");
+const idadeComprador = 24;
+
+if(idadeComprador <18){
+  console.log("Infelizmente, nao podemos vender as passagens para menor de idade");
+}
+if(idadeComprador >= 18){
+  console.log("Parabens! Voce adquiriu sua passagem com sucesso.");
+}
